86035f3e3bb028a2de0d2ec04b6a90129bb3370b
Alison Shin <alisonyshin@gmail.com>
2018-08-01 02:25:24 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   your_hash = {
-    "name" => "Alison"
+    "name" => "Alison",
     "age" => 27
   }
 end
@@ -8,9 +8,9 @@ end
 
 def shipping_manifest
   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2
-    
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" =>3
   }
 
 end
@@ -22,7 +22,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -33,11 +33,11 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
